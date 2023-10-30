dcd485f90be7d1bdf51725c36c5202a21df3e9d5
Amit Kumar Yadav <49604010+amit1github@users.noreply.github.com>
2023-10-30 16:30:42 +0530

    feat: controllers

--- controllers/book.js
+++ controllers/book.js
@@ -1,18 +1,98 @@
-const Book = require("../models/book")
+const Book = require("../models/book");
 
-exports.getAllBooks = async(req, res) => {
+// @method GET
+// @route /api/v1/book
+// @desc Retrieve all books
+exports.getAllBooks = async (req, res) => {
+  try {
+    const books = await Book.find();
+    res.status(200).json(books);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Error retrieving books" });
+  }
+};
 
-}
+// @method POST
+// @route /api/v1/book
+// @desc Add new Book.
+exports.addNewBook = async (req, res) => {
+  try {
+    const { title, author, summary } = req.body;
 
-exports.addNewBook = async(req, res) => {
+    const newBook = new Book({
+      title,
+      author,
+      summary,
+    });
 
-}
-exports.getBookById = async(req, res) => {
+    const savedBook = await newBook.save();
+    res.status(201).json(savedBook);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Error adding a book" });
+  }
+};
 
-}
-exports.UpdateBookDetails = async(req, res) => {
+// @method GET
+// @route /api/v1/book/:id
+// @desc Retrieve a Single Book with ID
+exports.getBookById = async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id);
 
-}
-exports.DeleteBook = async(req, res) => {
+    if (!book) {
+      res.status(404).json({ error: "Book not found" });
+      return;
+    }
 
-}
+    res.status(200).json(book);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Error retrieving the book" });
+  }
+};
+
+// @method PUT
+// @route /api/v1/book/:id
+// @desc Update a Book details with ID
+exports.UpdateBookDetails = async (req, res) => {
+  try {
+    const { title, author, summary } = req.body;
+
+    const updatedBook = await Book.findByIdAndUpdate(
+      req.params.id,
+      { title, author, summary },
+      { new: true }
+    );
+
+    if (!updatedBook) {
+      res.status(404).json({ error: "Book not found" });
+      return;
+    }
+
+    res.status(200).json(updatedBook);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Error updating the book" });
+  }
+};
+
+// @method DELETE
+// @route /api/v1/book/:id
+// @desc Delete Book with ID.
+exports.DeleteBook = async (req, res) => {
+  try {
+    const deletedBook = await Book.findByIdAndDelete(req.params.id);
+
+    if (!deletedBook) {
+      res.status(404).json({ error: "Book not found" });
+      return;
+    }
+
+    res.status(204).send(); // 204 No Content for successful deletion
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Error deleting the book" });
+  }
+};
--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const {getAllBooks, addNewBook, getBookById, UpdateBookDetails, DeleteBook } = require("../controllers/book")
+const {
+  getAllBooks,
+  addNewBook,
+  getBookById,
+  UpdateBookDetails,
+  DeleteBook,
+} = require("../controllers/book");
 
 // @route Retrieve all books
 router.get("/", getAllBooks);
@@ -13,9 +19,9 @@ router.post("/", addNewBook);
 router.get("/:id", getBookById);
 
 // @route Update a Book details with ID
-router.put("/:id", UpdateBookDetails)
+router.put("/:id", UpdateBookDetails);
 
 // @route Delete Book with ID
-router.delete("/:id", DeleteBook)
+router.delete("/:id", DeleteBook);
 
 module.exports = router;
